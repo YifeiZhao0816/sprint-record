14b341d7e70ab65ed38852c1b64c2b4ba70b5681
赵逸飞 <zhaoyifei2001@outlook.com>
2019-11-09 20:53:51 -0500

    initial build

--- SprintRecord/SprintRecord/Models/Team.cs
+++ SprintRecord/SprintRecord/Models/Team.cs
@@ -4,11 +4,26 @@ namespace SprintRecord.Models
 {
     public class Team
     {
-        public List<SprintWeek> Sprints { get;  }
         public string Name { get; set; }
         public int AvgVelocity { get; set; }
-        public int MyProperty { get; set; }
+        public int TotalHrs { get; set; }
         public int SprintPercent { get; set; }
+        public Dictionary<string, int[]> MemberStatus { get;  }
+        public List<SprintWeek> Sprints { get; }
+
+        public Team(string name, IEnumerable<string> members)
+        {
+            AvgVelocity = int.MinValue;
+            TotalHrs = int.MinValue;
+            SprintPercent = int.MinValue;
+            Name = name;
+            Sprints = new List<SprintWeek>();
+            MemberStatus = new Dictionary<string, int[]>();
+            foreach (var member in members)
+            {
+                MemberStatus.Add(member, new int[2] { int.MinValue, int.MinValue });
+            }
+        }
 
     }
 }
