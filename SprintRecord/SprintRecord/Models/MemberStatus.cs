213c2c20610cfdb74dd669265c996c4ac0ee1ae1
Rob Higginbotham <higginbothar@Robs-MacBook-Pro.local>
2019-12-09 17:03:40 -0500

    All the junk Rob did

---
+++ SprintRecord/SprintRecord/Models/MemberStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SprintRecord.Models
+{
+    public class MemberStatus
+    {
+        public string Name { get; set; }
+        public int WorkHours { get; set; }
+        public int SprintHours { get; set; }
+    }
+}
