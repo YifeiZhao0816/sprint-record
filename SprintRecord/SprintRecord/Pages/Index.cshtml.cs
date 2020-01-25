4d72a21449a9d4b36c07117ccdeb571f784e92ad
赵逸飞 <zhaoyifei2001@outlook.com>
2020-01-25 14:20:16 -0500

    db

--- SprintRecord/SprintRecord/Pages/Index.cshtml.cs
+++ SprintRecord/SprintRecord/Pages/Index.cshtml.cs
@@ -5,18 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+using SprintRecord.Models;
 
 namespace SprintRecord.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public SprintContext dbContext { get; set; }
+        
 
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, SprintContext context)
         {
             _logger = logger;
+            dbContext = context;
         }
 
         public void OnGet()
